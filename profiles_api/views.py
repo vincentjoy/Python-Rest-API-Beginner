4a4915f81ccab03209a4412cd085f2b7b2cbfde5
Rehlat Online Services Pvt Ltd <joy.vincent@rehlat.com>
2021-08-21 19:59:28 +0530

    added api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API view"""
+
+    def get(self, request, format=None):
+        """Returns a list of API View features"""
+
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional django view',
+            'Gives you the most control over your applicatiopn logic',
+            'Is mapped manually to your URLs'
+        ]
+
+        return Response({
+            'message': 'Hello!',
+            'an_apiview': an_apiview
+        })
